815ddc10071a635095a423f438bba5e9a0cb168d
Gieandes Silva <contato@gieandessilva.com>
2018-09-11 22:47:07 -0300

    Projeto AluraPic - Módulo 1

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+import {PhotoListModule} from './photo-list/photo-list.module';
+import {PhotoFormModule} from './photo-form/photo-form.module';
+import { PhotoModule } from './photo/photo.module';
+
+@NgModule({
+    imports: [ PhotoModule, HttpClientModule, CommonModule, PhotoListModule, PhotoFormModule]
+})
+export class PhotosModule {}
